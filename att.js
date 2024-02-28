4c77245a08c063cfcb71477c6c6f4597b0340d91
Daniil <daniil424342@gmail.com>
2024-02-28 15:00:48 +0600

    function new test

--- att.js
+++ att.js
@@ -4,3 +4,19 @@ function speak(text) {
     message.text = text;
     window.speechSynthesis.speak(message);
 }
+
+
+
+
+
+function myFunc(theObject) {
+    theObject.make = "Toyota";
+}
+
+const mycar = { make: "Honda", model: "Accord", year: 1998 };
+let x, y;
+
+x = mycar.make; 
+
+myFunc(mycar);
+y = mycar.make
